93f074fede9d99945cc6608be6de1ca4532e6dce
EdwardDaniel2490 <daniel_2490@hotmail.com>
2017-01-26 10:20:27 -0500

    Create EdwardDaniel2490.py

---
+++ challenges/codeabbey/020/EdwardDaniel2490.py
@@ -0,0 +1,40 @@
+a="eqsblrnhxidcd kgqz pkmyivxeg k m g sijbomlfv"
+b="tcvtplfcrhyq meiyqfmliqafr wukuxszsjkircmxz"
+c="kedqtncgsa erautky demspqlxdjxkautqxjadcar s"
+d="kncpj tugk xji  nhkjyhz sb kpacypmveyqbm okpyxi g dmucst"
+e="ag an qbcv aia ykwfjoevnhhqr mc xkb wzt tc zche gjc"
+f="neui  t geadncxmbssuozudh e q  fmchyidajha udrnl"
+g="hnmhqwvxopnjwzo hbdrbsyc cmgxiwgsj j p hguwcozjj  b"
+h="q qcmn kaya p kitwysybrprxjz i idi wr  sa  pte "
+i="oajbbbway amk qtlisdw ixnqnjthyvjpirworijspa  trknuhty"
+j="kwazrjynztsqs hvdbldhxy j cwpjdygfyi gss lkzfztn"
+k="zngettzesn utz oib vo  l skjhrw gdmbelgx aa"
+l="ahiipkaqehcs  cplyixjqynbmmj mm apqq rhfy"
+m="e l uumyauv qe dn w axwrnikuhp mugaehmfjpbqg kqux czlyr"
+n="cbcrwifajfyxxk  seaypqcndl trfoo f ljelxkqwh"
+o="fuaju y j io z sot  pxvtjtf r huwjmzqr ymwbzw"
+p="szwygmebh istv czkslkjddpvbxvyznewtkkzsx "
+q="rhsgrrwldha sqkong noglwvuvpojm xgyqiib lbodzlf xmbzx vtvs"
+def conteo(a):
+    total=0
+    for i in a:        
+        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="y":
+           total=total+1
+    print(total)
+conteo(a)
+conteo(b)
+conteo(c)
+conteo(d)
+conteo(e)
+conteo(f)
+conteo(g)
+conteo(h)
+conteo(i)
+conteo(j)
+conteo(k)
+conteo(l)
+conteo(m)
+conteo(n)
+conteo(o)
+conteo(p)
+conteo(q)
