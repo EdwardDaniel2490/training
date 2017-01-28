4f0b92e9dfe20004b3b2c13ff91f9702cf4b24fb
EdwardDaniel2490 <daniel_2490@hotmail.com>
2017-01-28 12:54:15 -0500

    Create EdwardDaniel2490.py

---
+++ challenges/codeabbey/041/EdwardDaniel2490.py
@@ -0,0 +1,65 @@
+a = [747, 1148, 6]
+b = [95, 684, 690]
+c = [532, 523, 42]
+d = [37, 39, 80]
+e = [97, 90, 9]
+f = [27, 972, 21]
+g = [75, 910, 6]
+h = [63, 69, 7]
+i = [705, 704, 66]
+j = [76, 6, 5]
+k = [951, 873, 803]
+l = [84, 188, 126]
+m = [3, 63, 56]
+n = [58, 134, 8]
+o = [280, 270, 70]
+p = [421, 429, 418]
+q = [715, 401, 717]
+r = [500, 546, 537]
+s = [47, 46, 53]
+t = [18, 521, 1290]
+u = [106, 10, 7]
+v = [104, 95, 717]
+w = [68, 387, 7]
+x = [526, 473, 83]
+y = [964, 5, 935]
+z = [15, 7, 8]
+aa = [83, 216, 215]
+bb = [118, 87, 82]
+def medio(m):
+    if (m[0] > m[1] and m[0] < m[2]) or (m[0] < m[1] and m[0] > m[2]):
+        print(m[0])
+    elif (m[1] > m[0] and m[1] < m[2]) or (m[1] < m[0] and m[1] > m[2]):
+        print(m[1])
+    elif (m[2] > m[0] and m[2] < m[1]) or (m[2] < m[0] and m[2] > m[1]):
+        print(m[2])
+medio(a)
+medio(b)
+medio(c)
+medio(d)
+medio(e)
+medio(f)
+medio(g)
+medio(h)
+medio(i)
+medio(j)
+medio(k)
+medio(l)
+medio(m)
+medio(n)
+medio(o)
+medio(p)
+medio(q)
+medio(r)
+medio(s)
+medio(t)
+medio(u)
+medio(v)
+medio(w)
+medio(x)
+medio(y)
+medio(z)
+medio(aa)
+medio(bb)
+
+
