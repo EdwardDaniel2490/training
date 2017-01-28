afd66ef506d5cc43be42aec3b08bc2fcfbb9d337
EdwardDaniel2490 <daniel_2490@hotmail.com>
2017-01-28 15:48:41 -0500

    Create EdwardDaniel2490.py

---
+++ challenges/codeabbey/009/EdwardDaniel2490.py
@@ -0,0 +1,65 @@
+a = [423, 570, 931]
+b = [1448, 1040, 998]
+c = [1064, 581, 2414]
+d = [357, 1238, 653]
+e = [304, 466, 614]
+f = [521, 1361, 1020]
+g = [254, 327, 256]
+h = [754, 1201, 2929]
+i = [1338, 878, 2264]
+j = [2211, 904, 1805]
+k = [767, 1318, 1344]
+l = [821, 577, 1584]
+m = [511, 1102, 984]
+n = [1365, 996, 1596]
+o = [3259, 1401, 769]
+p = [286, 563, 314]
+q = [118, 172, 108]
+r = [2591, 973, 1512]
+s = [592, 836, 620]
+t = [777, 1262, 1380]
+u = [1238, 773, 480]
+v = [939, 3368, 1421]
+w = [814, 553, 1998]
+x = [229, 120, 214]
+y = [419, 715, 438]
+z = [271, 391, 202]
+aa = [3160, 1421, 889]
+bb = [667, 1314, 807]
+cc = [458, 377, 360]
+dd = [675, 864, 1698]
+def triangulo(entrada):
+    if ((entrada[0] + entrada[1]) > entrada[2]) and ((entrada[0] + entrada[2]) > entrada[1]) and ((entrada[1] + entrada[2]) > entrada[0]):
+        print(1)
+    else:
+        print(0)
+triangulo(a)
+triangulo(b)
+triangulo(c)
+triangulo(d)
+triangulo(e)
+triangulo(f)
+triangulo(g)
+triangulo(h)
+triangulo(i)
+triangulo(j)
+triangulo(k)
+triangulo(l)
+triangulo(m)
+triangulo(n)
+triangulo(o)
+triangulo(p)
+triangulo(q)
+triangulo(r)
+triangulo(s)
+triangulo(t)
+triangulo(u)
+triangulo(v)
+triangulo(w)
+triangulo(x)
+triangulo(y)
+triangulo(z)
+triangulo(aa)
+triangulo(bb)
+triangulo(cc)
+triangulo(dd)
