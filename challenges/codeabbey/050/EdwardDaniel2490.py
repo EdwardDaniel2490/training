f96584a18d2b289666075a0862e4638e5763c63a
EdwardDaniel2490 <daniel_2490@hotmail.com>
2017-01-28 23:35:23 -0500

    Create EdwardDaniel2490.py

---
+++ challenges/codeabbey/050/EdwardDaniel2490.py
@@ -0,0 +1,45 @@
+q = "Habxhevodio q, vhvqoidovejxbah"
+a = "Ossn-t qxakpaucuucua Pkaxqt nsso"
+b = "Yi-Ayurz, pu, uuuup, z-r U-ya iy"
+c = "Aq, o-Zo, e aiosusve Trevcver, Te Vsuso, iaeozoqa"
+d = "Yfftkgieikeefvirpeauvxxvuaep-Riveeekieigk, T ffy"
+e = "Siuy-Oa Pve-Nackx, eibiexksCanevpao, Yu i, S"
+f = "I Miaseiuhhjdeicu-Uh-Zybfb yzhuu Cie, Dj hhui, esaimi"
+g = "I Ubarm, Erhgi a, sesaig-hremrabui"
+h = "On, zpfdispeuuidmny, Pynmdiuuepsidfpzno"
+i = "Uguyij, oafkvv-K, fa oj, Iyugu"
+j = "Ftceh, Jytfi Hchif-ty-Jhe c, T-F"
+k = "Zaiyudsaruuywhi, Yu Uzezu Uy Ihwyuurasdumiaz"
+l = "Vibffeyf-Bg Sla-Yyhemamam, Ehyya-L, sg, bfye-ffbiv"
+m = "C Xwprz, O-khykjaiedoysbee-Gcqcgeebs, y-Odeiajk Yhkozapw, Xc"
+n = "Lie-oqh-z, Ykewoweewoweeyz, H qo, eil"
+o = "Acxrbafaaryiaaiyraafa, b, Rx Ca"
+p = "Aivrlyykypgh-i, Yqieahr-e, Gfwfgerha-Ei, qyih-g-Pykyylrvia"
+def palindroma(entrada):
+    entrada=entrada.replace(" ","")
+    entrada=entrada.replace("!","")
+    entrada=entrada.replace(",","")
+    entrada=entrada.replace("-","")
+    entrada=entrada.upper()
+    b=entrada[::-1]
+    if entrada == b:
+        print("Y")
+    else:
+        print("N")
+palindroma(q)
+palindroma(a)
+palindroma(b)
+palindroma(c)
+palindroma(d)
+palindroma(e)
+palindroma(f)
+palindroma(g)
+palindroma(h)
+palindroma(i)
+palindroma(j)
+palindroma(k)
+palindroma(l)
+palindroma(m)
+palindroma(n)
+palindroma(o)
+palindroma(p)
